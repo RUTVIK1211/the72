13bc72b0ff3351f674edc28e766871c0aa10ebd0
manthan998 <72962337+manthan998@users.noreply.github.com>
2020-10-16 11:28:23 +0530

    GUI 1.0 relese
    the first commit with layout design

---
+++ import/header.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 3 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini layout-navbar-fixed layout-fixed">
+<div class="wrapper">
+
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+   
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+     
+        <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+         <i class="fas fa-users mr-2"></i>
+          
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+          
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-sign-out-alt"></i> Log out
+    
+          </a>
+        </div>
+      </li>
+   
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+   
+
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v1</p>
+                </a>
+              </li>
+             
+            </ul>
+          </li>
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon far fa-circle text-info"></i>
+              <p>Informational</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    
+    <!-- /.content-header -->
---
+++ insert.php
@@ -0,0 +1,437 @@
+ <?php
+
+ require 'import/header.php';
+
+?>
+
+ <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">insert</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+	
+   <!-- Main content -->
+    <section class="content">
+      <div class="container">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-warning">
+              <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              
+              <div class="container">
+			<form action="insq.php" method="post" >
+				<div class="row">
+					<div class="form-group col">
+						<label for="1">Date Actuelle</label>
+						<input type="date" class="form-control" id="input" name="D1" value="">
+					</div>
+					<div class="form-group col">
+						<label for="2">societe</label>
+						<input type="text" class="form-control" id="input" name="D2" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="3">Agence maritime</label>
+						<input type="time" class="form-control" id="input" name="D3" value="">
+					</div>
+					<div class="form-group col">
+						<label for="4">Numbero BL</label>
+						<input type="number" class="form-control" id="input" name="D4" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="5">Numbero CO</label>
+						<input type="number" class="form-control" id="input" name="D5" value="">
+					</div>
+					<div class="form-group col">
+						<label for="6">Date Reception BL</label>
+						<input type="date" class="form-control" id="input" name="D6" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="7">Date Arrivee Co</label>
+						<input type="date" class="form-control" id="input" name="D7" value="">
+					</div>
+					<div class="form-group col">
+						<label for="8">Remaining Day Avant Arrivee CO</label>
+						<input type="text" class="form-control" id="input" name="D8" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="9">Date DE Legalistation 7 Jours avant arrivee CO</label>
+						<input type="date" class="form-control" id="input" name="D9" value="">
+					</div>
+					<div class="form-group col">
+						<label for="10">Date De sortie CO Port Luanda 4 jours apres date arrivee</label>
+						<input type="date" class="form-control" id="input" name="D10" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="11">Date DE Sortie Reelle</label>
+						<input type="date" class="form-control" id="input" name="D11" value="">
+					</div>
+					<div class="form-group col">
+						<label for="12">Jours DE Penalites Date DE Sortie Reelle Moins Date DE sortie Normale</label>
+						<input type="date" class="form-control" id="input" name="D12" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="13">Date DE Retour CO</label>
+						<input type="date" class="form-control" id="input" name="D13" value="">
+					</div>
+					<div class="form-group col">
+						<label for="14">Date Reelle DE Retour CO</label>
+						<input type="date" class="form-control" id="input" name="D14" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="15">Jours DE Penalites Date DE Retour CO Reelle Moins Date DE Retour Normale</label>
+						<input type="date" class="form-control" id="input" name="D15" value="">
+					</div>
+					<div class="form-group col">
+						<label for="16">Type DE</label>
+						<input type="text" class="form-control" id="input" name="D16" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="17">POIDS</label>
+						<input type="text" class="form-control" id="input" name="D17" value="">
+					</div>
+					<div class="form-group col">
+						<label for="18">Nombre DE Colis</label>
+						<input type="text" class="form-control" id="input" name="D18" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="19">Nature marchandise</label>
+						<input type="text" class="form-control" id="input" name="D19" value="">
+					</div>
+					<div class="form-group col">
+						<label for="20">Camions DE Transport </label>
+						<input type="text" class="form-control" id="input" name="D20" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="21"> NOM DU CHAUFFEUR</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="22">PLAQUE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="23">TELEPHONE</label>
+						<input type="number" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="24">JOUR OU IL QUITTE LUANDA </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="25">JOUR ARRIVEE A LUFU </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="26"> JOUR DEPOTAGE</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="27">ARTICLES RETIRES PAR OCC </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="28">ARTICLES RETIRES PAR VERIFICATION  </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="29"> ARTICLES RETIRES PAR SANTE</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="30">  ARTICLES RETIRES PAR AUTRE 31 SOLDE DU NOMBRE ARTICLES</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="31">SOLDE DU NOMBER ARTICLES </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="32">DATE ARRIVEE A KIN  </label>
+						<input type="date" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="33">DATE DEPART DE KIN </label>
+						<input type="date" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="34">DATE ARRIVEE A LUANDA  </label>
+						<input type="date" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="35">RETOUR CO ET INTERCHANGE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="36"> COUT DE LEGALISATION ET NUMERO FACTURE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="37">COUT DE DOUANE ANGOLA </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="38">COUT DE PORT LUANDA </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="39"> COUT DE TERMINALE LUANDA </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="40">AUTRES DEPENSES </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="41"> COUT DE TRANSPORT </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="42"> COUT DE PASSA DE TRAVERSIA</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="43">COUT DE FUMIGATION </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="44"> COUT DE POLICE FRONTIERE  </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="45"> COUT DE SANTE FRONTIERE</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="46"> COUT DE FONER </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="47"> IMMIGRATION DGM </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="48"> FERI</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="49"> DOUANE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="50">NUMERO E DOUANE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="51"> BULLETIN</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="52">OCC LABO </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="53"> REDRESSEMENT OCC </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="54"> CAUTION CO</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="55">PEAGE LUFU </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="56">  PEAGE NOKI</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="57"> PEAGE KENGE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="58">  PEAGE KASANGULU </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="59"> ASSURANCE</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="60"> FEUILLE DE ROUTE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="61">MITENDI </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="62"> ANTI FRAUDE</label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="63"> DOUANE VILLE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="64">BDLF </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="65"> PONT PESE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="66">CAUTION 1 </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="67"> CAUTION 2 </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="68">FACTURE CLIENT </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="69">REMBOURSEMENT CLIENT </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="70">AVANCE SUR FACTURE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col">
+						<label for="71">SOLDE SUR FACTURE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+					<div class="form-group col">
+						<label for="72"> MARGE SOCIETE </label>
+						<input type="text" class="form-control" id="input" name="" value="">
+					</div>
+				</div>
+				<div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+				
+				
+				<div class="row">
+					<div class="form-group col-md-12">
+						<input type="submit" value="submit" name="submit" class="btn btn-outline-info">
+						<input type="reset" value="reset" class="btn btn-outline-danger">
+					</div>
+				</div>
+			</form>
+		</div>
+            </div>
+            <!-- /.card -->
+
+           
+
+          </div>
+     
+            
+          </div>
+          <!--/.col (right) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+
+
+
+ <?php
+
+ require 'import/footer.php';
+
+?>
