bcfb7a8f5210e3b15d2798445732a379ac6efb78
Rutvik <bhimani.rutvik1211@gmail.com>
2020-10-16 23:43:22 +0530

    removed unwnated

--- import/header.php
+++ import/header.php
@@ -4,19 +4,19 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
-<head>
-   <!-- Tell the browser to be responsive to screen width -->
+  <head>
+    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"> -->
     <link rel="stylesheet" href="dist/css/my.css">
     <!-- DataTables -->
     <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-      <!-- SweetAlert2 -->
+    <!-- SweetAlert2 -->
     <link rel="stylesheet" href="plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <!-- iCheck -->
     <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
@@ -25,66 +25,57 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
     <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+    <!-- <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css"> -->
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-</head>
-<body class="hold-transition layout-top-nav layout-navbar-fixed" >
-<div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md bg-teal disabled color-palette navbar-dark">
-    <div class="container">
-      <a href="index.php" class="navbar-brand">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-      </a>
-      
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="index.php" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="insert.php" class="nav-link">Insert</a>
-          </li>
-          <li class="nav-item">
-            <a href="view.php" class="nav-link">View</a>
-          </li>
-        </ul>
-       </div>
-
-      <!-- Right navbar links -->
-      <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-      
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-              <i class="fas fa-users mr-2"></i>
-
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-              <div class="dropdown-divider"></div>
-              <a href="change-password.html" class="dropdown-item">
-                <i class="fa fa-key" aria-hidden="true"> </i>&nbsp;Change password
-
+  </head>
+  <body class="hold-transition layout-top-nav layout-navbar-fixed" >
+    <div class="wrapper">
+      <!-- Navbar -->
+      <nav class="main-header navbar navbar-expand-md bg-teal disabled color-palette navbar-dark">
+        <div class="container">
+          <a href="index.php" class="navbar-brand">
+            <span class="brand-text font-weight-light">AdminLTE 3</span>
+          </a>
+          <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="index.php" class="nav-link">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href="insert.php" class="nav-link">Insert</a>
+              </li>
+              <li class="nav-item">
+                <a href="view.php" class="nav-link">View</a>
+              </li>
+            </ul>
+          </div>
+          <!-- Right navbar links -->
+          <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+              <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fas fa-users mr-2"></i>
               </a>
-              <div class="dropdown-divider"></div>
-              <a href="#" class="dropdown-item">
-                <i class="fas fa-sign-out-alt"></i> Log out
-
-              </a>
-            </div>
-          </li>
-      </ul>
-    </div>
-  </nav>
-  <!-- /.navbar -->
+              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <div class="dropdown-divider"></div>
+                <a href="change-password.html" class="dropdown-item">
+                  <i class="fa fa-key" aria-hidden="true"> </i>&nbsp;Change password
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-sign-out-alt"></i> Log out
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- /.navbar -->
