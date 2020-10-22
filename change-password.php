d88c94df15f05c2e3fec85241a41ba86772e6f51
Rutvik <bhimani.rutvik1211@gmail.com>
2020-10-22 22:20:06 +0530

    final remoeved

--- change-password.php
+++ change-password.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -19,7 +19,7 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition login-page bg-teal">
-<div class="login-box"> 
+<div class="login-box">
   <div class="login-logo text-primary">
     <a href="login.html"><b>Admin</b>LTE</a>
   </div>
